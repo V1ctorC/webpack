5564558b0c4329d5b18e93a50916529c1e67f4cc
Victor <victor.du.77@hotmail.fr>
2019-06-16 19:03:42 +0200

    Course 11

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,6 +3,9 @@
 const $ = require('jquery');
 require('bootstrap');
 
+//Pour faire fonctionner les promesses (ES6)
+require('babel-polyfill');
+
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
