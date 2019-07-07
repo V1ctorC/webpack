28a2d303c6543d1be052f103e3e4214d2b81fa45
Victor <victor.du.77@hotmail.fr>
2019-07-07 12:41:36 +0200

    Course 15

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -4,6 +4,7 @@ const $ = require('jquery');
 require('bootstrap');
 
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/main.css');
 
 //Pour faire fonctionner les promesses (ES6)
