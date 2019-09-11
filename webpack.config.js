b2bf871cf3f1e1541bce4f6a799de5088361fc95
Victor <victor.du.77@hotmail.fr>
2019-09-11 19:56:30 +0200

    Course 32

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,21 @@ const CopyPlugin = require('copy-webpack-plugin');
 const useDevServer = false;
 const publicPath = useDevServer ? 'http://localhost:8080/build/' : '/build/';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const useSourcemaps = !isProduction;
 
 
 const styleLoader =  {
     loader: 'style-loader',
     options: {
-        sourceMap: true
+        sourceMap: useSourcemaps
     }
 };
 const cssLoader =  {
     loader: 'css-loader',
     options: {
-        sourceMap: true
+        sourceMap: useSourcemaps,
+        minimize: isProduction
     }
 };
 const sassLoader =  {
@@ -28,7 +31,7 @@ const sassLoader =  {
 const resolveUrlLoader =  {
     loader: 'resolve-url-loader',
     options: {
-        sourceMap: true
+        sourceMap: useSourcemaps
     }
 };
 
@@ -121,18 +124,31 @@ const webpackConfig = {
 
         new ExtractTextPlugin('[name].css'),
     ],
-    devtool: "inline-source-map",
+    devtool: useSourcemaps ? "inline-source-map" : false,
     devServer: {
         contentBase: './web',
         headers: { 'Access-Control-Allow-Origin': '*' },
     }
 };
 
-if (process.env.NODE_ENV === 'production')
+if (isProduction)
 {
     webpackConfig.plugins.push(
         new webpack.optimize.UglifyJsPlugin(),
-    )
+    );
+
+    webpackConfig.plugins.push(
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        })
+    );
+
+    webpackConfig.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    );
 }
 
 module.exports = webpackConfig;
